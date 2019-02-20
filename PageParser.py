749f20185ccc3b51ba54edc120b69f2d25f43994
Meng Xiangzhuo <xzmong@icloud.com>
2019-02-20 18:08:09 +0800

    fix re.

--- PageParser.py
+++ PageParser.py
@@ -14,7 +14,7 @@ class PageParser(object):
         intro = soup.select('div.para[label-module=para]')[0].get_text()
         # <a target="_blank" href="/item/%E9%98%BF%E5%A7%86%
         new_full_urls = set()
-        new_urls = soup.find_all(href=re.compile(r'/item/*'))
+        new_urls = soup.find_all(href=re.compile(r'/item/.+'))
         for a in new_urls:
             rel_url = a['href']
             full_url = urljoin(new_url, rel_url)
